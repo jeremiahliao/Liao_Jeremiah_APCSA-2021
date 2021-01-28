20be8771e2add755e3ad2095367a7b7946f3c8c8
jeremiahliao <78109874+jeremiahliao@users.noreply.github.com>
2021-01-28 12:33:19 -0800

    Unit 00 initial push - Ascii Box

---
+++ Unit0/src/AsciiBox.java
@@ -0,0 +1,33 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Jeremiah Liao
+//Date - 1-28-2021
+//Class - APCSA - Mr. Mauro
+//Lab  - 0a 1.output - Ascii Box
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("name: Jeremiah Liao \t  date: 1-28-2021 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+		System.out.println("+++++++++++++++++++++++++ ");
+	}
+}
