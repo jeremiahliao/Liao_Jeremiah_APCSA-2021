5e77cb39a14e21654a9c03660d7b90d705f9ce06
jeremiahliao <78109874+jeremiahliao@users.noreply.github.com>
2021-01-28 17:30:48 -0800

    Unit 00 upload 2 - ascii art

---
+++ Unit0/src/AsciiArt.java
@@ -0,0 +1,36 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Jeremiah Liao
+//Date - 1-28-2021
+//Class -APCSA - Mr. Mauro
+//Lab  - Ascii Art
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Your Name: Jeremiah Liao \n\n" );
+		System.out.println("What type of ANIMAL YOU WILL DRAW : An Ant" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("             \\     /            " );
+		System.out.println("              \\___/               " );
+		System.out.println("         \\  /     \\   /          " );
+		System.out.println("          \\ |      |  /           " );
+		System.out.println("           --\\_____/--             " );
+		System.out.println("             /     \\             " );
+		System.out.println("          ---|      |---             " );
+		System.out.println("             \\_____/             " );
+		System.out.println("             /      \\             " );
+		System.out.println("           __|      |__    " );
+		System.out.println("          /  |      |  \\     " );
+		System.out.println("              \\____/             " );
+		//add other output
+
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
