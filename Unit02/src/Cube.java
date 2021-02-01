855c07ec6797fd0ce2d065763477e6382ca5ce22
jeremiahliao <78109874+jeremiahliao@users.noreply.github.com>
2021-02-01 12:26:06 -0800

    Unit 02 Cube Push

---
+++ Unit02/src/Cube.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -
+//Date -
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		long sideArea = side*side;
+		long surfaceArea = 6 * sideArea;
+		return(surfaceArea);
+	}
+}
