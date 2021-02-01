da7dbff231e96d1c82d16b9fcc2ca94d3dd27f74
jeremiahliao <78109874+jeremiahliao@users.noreply.github.com>
2021-01-31 22:48:05 -0800

    Unit 01 Initial Push

---
+++ Unit01/src/StarsAndStripes.java
@@ -0,0 +1,60 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      System.out.println("StarsAndStripes");
+      
+   }
+
+   public void printTwentyStars()
+   {
+	   System.out.println("********************");
+   }
+
+   public void printTwentyDashes()
+   {
+	   System.out.println("--------------------");
+   }
+
+   public void printTwoBlankLines()
+   {
+	   System.out.println("  ");
+   }
+   
+   public void printASmallBox()
+   {	
+	   System.out.println("_______");
+	   System.out.println("|     |");
+	   System.out.println("-------");
+   }
+ 
+   public void printABigBox()
+   { 	
+	   System.out.println("_______________");
+	   System.out.println("|             |");
+	   System.out.println("|             |");
+	   System.out.println("|             |");
+	   System.out.println("---------------");
+   }   
+   public void printACross()
+   {
+	   System.out.println("--------------------");
+	   System.out.println("-----------   ------");
+	   System.out.println("-----------   ------");
+	   System.out.println("---               --");
+	   System.out.println("---               --");
+	   System.out.println("-----------   ------");
+	   System.out.println("-----------   ------");
+	   System.out.println("--------------------");
+   }
+}
---
+++ Unit01/src/StarsRunner.java
@@ -0,0 +1,33 @@
+//� A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsRunner
+{
+   public static void main(String args[])
+   {
+      //instantiate a StarsAndStripes object
+      StarsAndStripes myStarsAndStripes = new StarsAndStripes();
+      
+      //call the methods needed to make the patterns on the word document
+      myStarsAndStripes.printTwentyStars();
+      myStarsAndStripes.printTwoBlankLines();
+      myStarsAndStripes.printTwentyStars();
+      myStarsAndStripes.printTwentyStars();
+      myStarsAndStripes.printTwentyStars();
+      myStarsAndStripes.printTwentyStars();
+      myStarsAndStripes.printACross();
+      myStarsAndStripes.printTwentyStars();
+      myStarsAndStripes.printTwentyStars();
+      myStarsAndStripes.printTwentyStars();
+      myStarsAndStripes.printTwentyStars();
+      myStarsAndStripes.printTwoBlankLines(); 
+      myStarsAndStripes.printTwentyStars();
+   }
+}
