8a2e8647388c34d2f1d76e33c959a8f9fcd3f25d
jeremiahliao <78109874+jeremiahliao@users.noreply.github.com>
2021-02-02 17:57:47 -0800

    Unit 03 MPH lab push

---
+++ Unit03/src/MPHRunner.java
@@ -0,0 +1,76 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class MPHRunner
+{
+	public static void main( String[] args )
+	{
+		Scanner keyboard = new Scanner(in);
+
+		out.print("Enter the distance :: ");
+		int dist = keyboard.nextInt();
+
+		out.print("Enter the hours :: ");
+		int hrs = keyboard.nextInt();
+
+		out.print("Enter the minutes :: ");
+		int mins = keyboard.nextInt();
+
+		MilesPerHour test = new MilesPerHour(dist, hrs, mins);
+		test.calcMPH();
+		test.print();
+		
+		//add more test cases
+		//case 2
+		out.print("Enter the distance :: ");
+		dist = keyboard.nextInt();
+
+		out.print("Enter the hours :: ");
+		hrs = keyboard.nextInt();
+
+		out.print("Enter the minutes :: ");
+		mins = keyboard.nextInt();
+		
+		MilesPerHour testOne = new MilesPerHour(dist,hrs, mins);
+		testOne.calcMPH();
+		testOne.print();
+		
+		//case 3
+		out.print("Enter the distance :: ");
+		dist = keyboard.nextInt();
+
+		out.print("Enter the hours :: ");
+		hrs = keyboard.nextInt();
+
+		out.print("Enter the minutes :: ");
+		mins = keyboard.nextInt();
+		
+		MilesPerHour testTwo = new MilesPerHour(dist,hrs, mins);
+		testTwo.calcMPH();
+		testTwo.print();
+		
+		//case 4
+		out.print("Enter the distance :: ");
+		dist = keyboard.nextInt();
+
+		out.print("Enter the hours :: ");
+		hrs = keyboard.nextInt();
+
+		out.print("Enter the minutes :: ");
+		mins = keyboard.nextInt();
+		
+		MilesPerHour testThree = new MilesPerHour(dist,hrs, mins);
+		testThree.calcMPH();
+		testThree.print();
+		
+	}
+}
---
+++ Unit03/src/MilesPerHour.java
@@ -0,0 +1,57 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import java.util.Scanner; 
+import static java.lang.System.*;
+import static java.lang.Math.*;
+
+public class MilesPerHour
+{
+	private int distance, hours, minutes;
+	private double mph;
+
+	public MilesPerHour()
+	{
+		setNums(0,0,0);
+		mph=0.0;
+	}
+
+	public MilesPerHour(int dist, int hrs, int mins)
+	{
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
+	}
+
+	public void setNums(int dist, int hrs, int mins)
+	{
+		distance = dist;
+		hours = hrs;
+		minutes = mins;
+	}
+
+	public void calcMPH()
+	{
+		double minutesConv = (double)minutes/60;
+		double time = hours + minutesConv;
+		mph = round(distance / time);
+		
+	}
+
+	public void print()
+	{
+		System.out.println(String.format("%d miles in %d hour and %d minutes = %.0f MPH", distance, hours, minutes, mph));
+	}
+	
+	//create a print or toString or both
+	
+	public String toString()
+	{
+		return "";
+	}
+}
