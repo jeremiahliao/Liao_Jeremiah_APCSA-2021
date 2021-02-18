41a5fee0b82e15d9899dfefad677ca80f34ed2d8
jeremiahliao <78109874+jeremiahliao@users.noreply.github.com>
2021-02-17 23:00:30 -0800

    Unit07 BiggestDouble push

--- Unit07/src/BiggestDouble.java
+++ Unit07/src/BiggestDouble.java
@@ -15,19 +15,32 @@ public class BiggestDouble
 
 	public BiggestDouble(double a, double b, double c, double d)
 	{
+		setDoubles(a,b,c,d);
 	}
 
 	public void setDoubles(double a, double b, double c, double d)
 	{
+		one = a;
+		two = b;
+		three = c;
+		four = d;
 	}
 
 	public double getBiggest()
 	{
-		return 0.0;
+		double bigger = one;
+		if(bigger < two)
+			bigger = two;
+		if(bigger < three)
+			bigger = three;
+		if(bigger < four)
+			bigger = four;
+		
+		return bigger;
 	}
 
 	public String toString()
 	{
-	   return "";
+	   return one + " " + two + " " + three + " " + four + "\nbiggest = " + getBiggest();
 	}
 }
--- Unit07/src/DoubleRunner.java
+++ Unit07/src/DoubleRunner.java
@@ -10,9 +10,12 @@ public class DoubleRunner
 	{
 		BiggestDouble run = new BiggestDouble(4.5,6.7,7.8,9.0);
 		System.out.println(run);
-		System.out.println("biggest = " + run.getBiggest() + "\n");
 
 		//add more test cases
-				
+		run.setDoubles(14.51, 6.17, 71.8, 1.0);		
+		System.out.println(run);
+		
+		run.setDoubles(4.5, -456.7, 677.8, 9.0);		
+		System.out.println(run);
 	}
 }
