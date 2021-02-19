20802759c2c6f7fa876f596219849fcafdd94b54
jeremiahliao <78109874+jeremiahliao@users.noreply.github.com>
2021-02-18 19:17:18 -0800

    Unit07 Triangle Push

--- Unit07/src/TriangleFive.java
+++ Unit07/src/TriangleFive.java
@@ -11,23 +11,47 @@ public class TriangleFive
 
 	public TriangleFive()
 	{
+		setLetter((char)0);
+		setAmount(0);
 	}
 
 	public TriangleFive(char c, int amt)
 	{
+		setLetter(c);
+		setAmount(amt);
 	}
 
 	public void setLetter(char c)
 	{
+		letter = c;
 	}
 
 	public void setAmount(int amt)
 	{
+		amount = amt;
 	}
 
 	public String toString()
 	{
 		String output="";
-		return output;
+		char modify = letter;
+		for(int i = amount;i > 0; i--)
+		{
+			for(int j = amount; j > amount - i; j--)
+			{
+				for(int k = j; k > 0 ; k--)
+				{
+					output += modify;
+				}
+				output += " ";
+				if(modify == 'Z')
+					modify = 'A';
+				else
+					modify = (char)(modify + 1);
+			}
+			output += "\n";
+			modify = letter;
+		}
+		return output + "\n";
 	}
 }
